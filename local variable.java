231b22c3299e0545ee87831e92b0b86c345d2313
cvasankaran <113054854+cvasankaran@users.noreply.github.com>
2022-09-08 13:53:54 +0530

    Add files via upload

---
+++ local variable.java
@@ -0,0 +1,15 @@
+//local variable
+class num
+{
+	Static void m()//method area
+	{
+		int x;
+		System.out.println(x);
+	}
+	public Static void main(String[] args)
+	{
+		System.out.println(x);
+	}
+}	
+	
+	
