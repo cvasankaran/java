59acef7c945ea5142cca5e2cc1dbe21bd3baef2b
cvasankaran <113054854+cvasankaran@users.noreply.github.com>
2022-10-27 23:02:11 +0530

    Add files via upload

---
+++ ReadString.java
@@ -0,0 +1,16 @@
+package siva;
+import java.io.*;  
+
+public class ReadString {
+
+	 
+	class ReadStringTest
+	{  
+	public static void main(String args[]){  
+	Console c=System.console();  
+	System.out.println("Enter your name: ");  
+	String n=c.readLine();  
+	System.out.println("Welcome "+n);  
+	}  
+	}
+}
